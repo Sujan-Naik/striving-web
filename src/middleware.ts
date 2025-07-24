a7943132f84aa91432d0939142d5db1c186495be
Sujan Naik <sujannaik047@gmail.com>
2025-07-24 13:10:16 +0100

    Implement protected routes using middleware and display calendar from Google

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,1 +1,13 @@
-export { auth as middleware } from "@/auth"
+import {auth} from "@/auth";
+import {NextResponse} from "next/server";
+
+export default auth((req) => {
+  if (!req.auth && req.nextUrl.pathname !== "/login") {
+    const newUrl = new URL("/login", req.nextUrl.origin)
+    return NextResponse.redirect(newUrl)
+  }
+})
+
+export const config = {
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+}
